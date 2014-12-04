5d733b85da7e1f8d5df557418e74313eb1aec09e
endermcbane <kccrookston@gmail.com>
2014-12-03 20:59:49 -0700

    second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+var imageCount = 1
+function nextPhoto () {
+		imageCount = imageCount+1;
+		document.getElementById('imageholder').style.backgroundImage = 'url("images/2.jpg")';
+		console.log(imageCount)
 
-}
+	};
+
+	$(".heading").on('click', '.navigation', function(){
+		nextPhoto();
+	});
+	
+
+});
